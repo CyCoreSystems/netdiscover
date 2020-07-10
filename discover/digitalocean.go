9f0d762cc05cd786d8fe5e3c65b7e39e72e14d42
Seán C McCord <ulexus@gmail.com>
2020-07-10 17:11:20 -0400

    Enable private IP discovery on DO
    Apparently, private IP discovery on DigitalOcean does work.
    
    Fixes #2
    
    Signed-off-by: Seán C McCord <ulexus@gmail.com>

--- discover/digitalocean.go
+++ discover/digitalocean.go
@@ -7,11 +7,11 @@ import (
 )
 
 const (
-	doHostnameURL = "http://169.254.169.254/metadata/v1/hostname"
-	//doPrivateIPv4URL = "http://169.254.169.254/metadata/v1/interfaces/private/0/ipv4/address"
-	doPublicIPv4URL = "http://169.254.169.254/metadata/v1/interfaces/public/0/ipv4/address"
-	//doFloatingPublicIPv4EnabledURL = "http://169.254.169.254/metadata/v1/floating_ip/ipv4/active"
-	//doFloatingPublicIPv4URL        = "http://169.254.169.254/metadata/v1/interfaces/public/0/anchor_ipv4/address"
+	doHostnameURL    = "http://169.254.169.254/metadata/v1/hostname"
+	doPrivateIPv4URL = "http://169.254.169.254/metadata/v1/interfaces/private/0/ipv4/address"
+	doPublicIPv4URL  = "http://169.254.169.254/metadata/v1/interfaces/public/0/ipv4/address"
+	// doFloatingPublicIPv4EnabledURL = "http://169.254.169.254/metadata/v1/floating_ip/ipv4/active"
+	// doFloatingPublicIPv4URL        = "http://169.254.169.254/metadata/v1/interfaces/public/0/anchor_ipv4/address"
 	doPublicIPv6URL = "http://169.254.169.254/metadata/v1/interfaces/public/0/ipv6/address"
 )
 
@@ -19,22 +19,19 @@ const (
 func NewDigitalOceanDiscoverer() Discoverer {
 	return NewDiscoverer(
 		PublicHostnameDiscovererOption(doHostname),
+		PrivateIPv4DiscovererOption(doPrivateIPv4),
 		PublicIPv4DiscovererOption(doPublicIPv4),
 		PublicIPv6DiscovererOption(doPublicIPv6),
 	)
-
 }
 
 func doHostname() (string, error) {
 	return StandardHostnameFromHTTP(doHostnameURL, nil)
 }
 
-// FIXME:  this URL never seems to be populated
-/*
 func doPrivateIPv4() (net.IP, error) {
 	return StandardIPFromHTTP(doPrivateIPv4URL, nil)
 }
-*/
 
 func doPublicIPv4() (net.IP, error) {
 	return StandardIPFromHTTP(doPublicIPv4URL, nil)
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/CyCoreSystems/netdiscover
 
-go 1.12
+go 1.14
